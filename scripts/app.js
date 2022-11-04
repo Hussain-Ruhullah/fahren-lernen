70eba45a00d856fef394e1cfde40081366f79994
Hussain Ruhullah <hruh@d-velop.de>
2022-11-04 23:16:04 +0100

    added icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 var startTime = 0;
 
 document.addEventListener("keydown", function (event) {
-	if(startTime === 0) {startTime = new Date().getTime()}
+	if (startTime === 0) { startTime = new Date().getTime() }
 })
 
 document.addEventListener("keyup", function (event) {
-	
+
 	let bookmark = document.getElementsByClassName("icon icon-IconQuestionMark")
 	let checkBoxes = document.getElementsByClassName("chk chk-answer")
 	let buttons = document.getElementsByClassName("btn btn-command")
@@ -16,7 +16,7 @@ document.addEventListener("keyup", function (event) {
 	let textAnswers = document.getElementsByClassName("audio-label");
 	let videoPlay = document.getElementsByClassName("icon icon-IconPlayVideo");
 	let goToTask = document.getElementsByClassName("icon-IconGoToTask");
-	
+
 	let playAudio = (startTime + 500) < (new Date().getTime()) ? true : false;
 	startTime = 0;
 
@@ -27,18 +27,19 @@ document.addEventListener("keyup", function (event) {
 	};
 
 	if (event.code === 'Numpad1') {
-		if (videoPlay.length>0) {
+		if (videoPlay.length > 0) {
 			videoPlay[0].click();
-		}else{
+		} else {
 			playAudio ? textAnswers[1].click() : checkBoxes[0].click();
 		}
 	};
 
 	if (event.code === 'Numpad2') {
-		if ( goToTask.length>0) {
+		if (goToTask.length > 0) {
 			goToTask[0].click();
-		}else{
-		playAudio ? textAnswers[2].click() : checkBoxes[1].click();}
+		} else {
+			playAudio ? textAnswers[2].click() : checkBoxes[1].click();
+		}
 	};
 
 	if (event.code === 'Numpad3') {
